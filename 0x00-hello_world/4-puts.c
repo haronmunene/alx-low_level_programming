04800322aa10193422356d21f1131431883384de
haronmunene <haronmunenemuriuki@gmail.com>
2023-03-16 11:40:14 -0700

    Using the puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include <stdio.h> 
+
 /**
  * main - A C program that prints a string using puts function
  * Return: 0 
  */
+
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
