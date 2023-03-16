1ef6158b2d86bb0cb5daa80497c8e69341e9a8e4
haronmunene <haronmunenemuriuki@gmail.com>
2023-03-16 11:17:43 -0700

    Using the puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,10 @@
-#!/bin/bash
-#include <stdio.h> int main() { puts ("\"Programming is like building a multilingual puzzle"); return 0;}
+#include <stdio.h> 
+/**
+ * C program that prints a string using puts function
+ * Return: 0 success
+ */
+int main()
+{
+puts ("\"Programming is like building a multilingual puzzle");
+return 0;
+}
