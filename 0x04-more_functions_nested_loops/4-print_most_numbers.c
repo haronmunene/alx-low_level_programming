2b422d48fc9c123a6cfa928f21373263322bc539
haronmunene <haronmunenemuriuki@gmail.com>
2023-03-25 04:50:52 -0700

    a function that prints most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - a function to print most numbers
+ *
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9 ; i++)
+		if (i != 2 && i != 4)
+			_putchar(i + '0');
+	_putchar('\n');
+}
