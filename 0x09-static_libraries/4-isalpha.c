7e12947200eece0323f788ec1f4d19547797c92e
haronmunene <haronmunenemuriuki@gmail.com>
2023-06-12 00:45:45 -0700

    creating static libraries

---
+++ 0x09-static_libraries/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks whether c is an alphabet, either in lowercase or uppercase
+ * @c: is the character being checked
+ * Return: 1 if character is an alphabet, otherwise 0
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _abs - computes absolute value of an integer
+ * @n: the value being checked
+ * Return: returns the value of an integer
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (-n);
+	}
+	else
+	{
+		return (n);
+	}
+}
